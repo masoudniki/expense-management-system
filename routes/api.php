d21283cbedf2234d2a247f8812015bc592bb543c
masoud nikzad <masoudniki79@gmail.com>
2024-03-04 10:00:40 +0330

    add login route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AttachmentController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExpenseRequestController;
 use App\Models\Expense;
 use Illuminate\Http\Request;
@@ -17,12 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/v1/expense-requests')->middleware('auth')->group(function (){
-    Route::get('/',[ExpenseRequestController::class,'list']);
-    Route::get('/types',[ExpenseRequestController::class,'types']);
-    Route::post('/',[ExpenseRequestController::class,'create']);
-    Route::post('/confirm',[ExpenseRequestController::class,'confirm'])->can('confirm',Expense::class);
-    Route::post('/reject',[ExpenseRequestController::class,'reject'])->can('reject',Expense::class);
-    Route::post('/pay-now',[ExpenseRequestController::class, 'payNow'])->can('payNow',Expense::class);
-    Route::get('/attachments/{attachment:uuid}',[AttachmentController::class,'download'])->name('attachment.download');
+Route::prefix('/v1/')->group(function (){
+
+    Route::prefix('auth')->group(function (){
+        Route::post('/login',[AuthController::class,'login']);
+    });
+
+    Route::prefix('expense-requests')->middleware('auth')->group(function (){
+        Route::get('/',[ExpenseRequestController::class,'list']);
+        Route::get('/types',[ExpenseRequestController::class,'types']);
+        Route::post('/',[ExpenseRequestController::class,'create']);
+        Route::post('/confirm',[ExpenseRequestController::class,'confirm'])->can('confirm',Expense::class);
+        Route::post('/reject',[ExpenseRequestController::class,'reject'])->can('reject',Expense::class);
+        Route::post('/pay-now',[ExpenseRequestController::class, 'payNow'])->can('payNow',Expense::class);
+        Route::get('/attachments/{attachment:uuid}',[AttachmentController::class,'download'])->name('attachment.download');
+    });
 });
